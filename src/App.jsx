26a5d91f58ab307d246401d2571f566b45583007
Esther <125416224+estheralv@users.noreply.github.com>
2024-12-10 19:28:15 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ListadeTarefas from './assets/components/ListadeTarefas'
+
+function App() {
+  return (
+    <>
+    <ListadeTarefas/>
+    </>
+  )
+}
+
+export default App
